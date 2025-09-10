90a3677106c9611d3761f15c7851df3bb246bb2e
Yahya Abou al magd <yahyaaboualmagd@Yahyas-MacBook-Air.local>
2025-09-09 18:17:54 -0700

    Fix module export in root entry point

--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 // Vercel entry point - exports the Express app for serverless deployment
-const app = require('./backend/dist/index.js').default;
-
-module.exports = app;
+module.exports = require('./backend/dist/index.js').default;
